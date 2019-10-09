f4a93614ffe4e23eb68e2f2b19a9350971415218
mariamiotek <Maria_Gomez@local.miotek.mad>
2019-10-09 14:37:26 +0200

    Login básico en angular, falta conectar firebase y la biblioteca Chart con nuevo modulo

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+
+import { RegistroComponent } from './pages/registro/registro.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NavbarComponent } from './compartido/navbar/navbar.component';
+import { FooterComponent } from './compartido/footer/footer.component';
+import { GraficasComponent } from './pages/graficas/graficas.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegistroComponent,
+    HomeComponent,
+    LoginComponent,
+    NavbarComponent,
+    FooterComponent,
+    GraficasComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
