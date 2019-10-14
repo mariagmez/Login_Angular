9118d26f0b309720b732a889bac61c78b1900da5
mariamiotek <Maria_Gomez@local.miotek.mad>
2019-10-14 16:05:11 +0200

    sweet alert configurado para recoger los errores

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { UsuarioModel } from 'src/app/models/usuario.model';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { switchAll } from 'rxjs/operators';
+
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,8 +17,8 @@ export class LoginComponent implements OnInit {
   usuario: UsuarioModel = new UsuarioModel();
   recordarme = false;
 
-  constructor( private auth: AuthService) { }
-
+  constructor( private auth: AuthService,
+    private router: Router ) { } 
   ngOnInit() {
   }
 
@@ -21,11 +26,28 @@ export class LoginComponent implements OnInit {
 
     if ( form.invalid){ return; }
 
+      Swal.fire({
+        allowOutsideClick: false,
+        type: 'info',
+        text: 'Espere...'
+      });
+      Swal.showLoading();
+
       this.auth.login(this.usuario)
       .subscribe(resp =>{
+
           console.log(resp);
+          Swal.close();
+          this.router.navigateByUrl('/home');
+
         }, (err)=>{
           console.log(err.error.error.message);
+          Swal.fire({
+            allowOutsideClick: false,
+            type: 'error',
+            title:'Datos incorrectos',
+            text: err.error.error.message
+          });
         });
   }
 }
--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { UsuarioModel } from 'src/app/models/usuario.model';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-registro',
@@ -13,7 +15,9 @@ export class RegistroComponent implements OnInit {
 
   usuario: UsuarioModel;
 
-  constructor( private auth: AuthService) { }
+  constructor( private auth: AuthService,
+    
+        private router: Router ) { }
 
   ngOnInit() { 
     this.usuario = new UsuarioModel();
@@ -23,12 +27,28 @@ export class RegistroComponent implements OnInit {
   onSubmit( form: NgForm ){
 
     if ( form.invalid ) { return;}
+
+    Swal.fire({
+      allowOutsideClick: false,
+      type: 'info',
+      text: 'Espere...'
+    });
+    Swal.showLoading();
+
       this.auth.nuevoUsuario(this.usuario)
       .subscribe( resp =>{
           console.log(resp);
+          Swal.close();
+          this.router.navigateByUrl('/home');
 
       }, (err) =>{
           console.log(err.error.error.message);
+          Swal.fire({
+            allowOutsideClick: false,
+            type: 'error',
+            title:'Datos incorrectos',
+            text: err.error.error.message
+          });
       } );
   }
 
